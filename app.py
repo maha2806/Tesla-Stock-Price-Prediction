2b910d8f0257c08138c1aaa0f69266c1d1018fdf
SITA MAHALAKSHMI BHARATULA <103115856+maha2806@users.noreply.github.com>
2026-06-09 13:24:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,10 @@ st.title("Tesla Stock Price Prediction")
 
 model = joblib.load("tesla_stock_prediction_model.pkl")
 
-st.write("Enter stock values")
-
 open_price = st.number_input("Open Price")
 high_price = st.number_input("High Price")
 low_price = st.number_input("Low Price")
-adj_close = st.number_input("Adj Close")
 volume = st.number_input("Volume")
-
-price_range = high_price - low_price
-
-daily_return = st.number_input("Daily Return")
-ma7 = st.number_input("MA 7")
-ma30 = st.number_input("MA 30")
 
 if st.button("Predict Closing Price"):
 
@@ -26,12 +17,7 @@ if st.button("Predict Closing Price"):
         open_price,
         high_price,
         low_price,
-        adj_close,
-        volume,
-        price_range,
-        daily_return,
-        ma7,
-        ma30
+        volume
     ]])
 
     prediction = model.predict(features)
